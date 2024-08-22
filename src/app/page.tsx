5e5e3a770b6699e1302a041b8128056ea5c98e41
abbas110 <meerabbas110@gmail.com>
2024-08-22 20:41:58 +0530

    applying those images on the screen and information text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,27 @@ export default function Home() {
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
             <div className="relative mx-auto flex flex-col items-center text-center lg:items-start lg:text-left">
               <div className="absolute -top-20 left-0 hidden w-28 lg:block">
-                image
+                <img src="/snake-1.png" className="w-full" alt="snake logo" />
               </div>
+              <h1 className="relative mt-16 w-fit text-balance text-5xl font-bold !leading-tight tracking-tight text-gray-900 md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 max-w-prose text-balance text-center text-lg md:text-wrap lg:pr-10 lg:text-left">
+                Capture your favorite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseCorner allows you to protect your memories, not just your
+                phone case.
+              </p>
+              <ul className="mt-8 flex flex-col items-center space-y-2 text-left font-medium sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex items-center gap-1.5 text-left">
+                    High-quality, durable material
+                    {/* lucide react icons */}
+                  </li>
+                </div>
+              </ul>
             </div>
           </div>
         </MaxWidthWrapper>
