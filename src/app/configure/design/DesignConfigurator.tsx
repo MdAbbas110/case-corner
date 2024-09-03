17af4dc69fd06bf82597bec2d51f1357d41d685b
nikunj <nikunjtiwari7@gmail.com>
2024-09-03 11:53:15 +0530

    Initial commit for setup

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,19 @@
+interface DesignConfiguratorProps{
+       configId:string
+       imgUrl:string
+       
+
+
+}
+
+const DesignConfigurator = ({configId,imageUrl,imageDimensions}: DesignConfiguratorProps) =>
+  {
+
+return 
+(
+   
+)
+
+}
+
+export default DesignConfigurator
---
+++ src/app/page.tsx
@@ -0,0 +1,170 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Phone from "@/components/Phone";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowRight, Check, Star } from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 sm:pb-32 lg:grid lg:grid-cols-3 lg:gap-x-0 lg:pb-52 lg:pt-24 xl:gap-x-8 xl:pt-32">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto flex flex-col items-center text-center lg:items-start lg:text-left">
+              <div className="absolute -top-20 left-0 hidden w-28 lg:block">
+                <img src="/snake-1.png" className="w-full" alt="snake logo" />
+              </div>
+              <h1 className="relative mt-16 w-fit text-balance text-5xl font-bold !leading-tight tracking-tight text-gray-900 md:text-6xl lg:text-7xl">
+                Your Image on a{" "}
+                <span className="bg-green-600 px-2 text-white">Custom</span>{" "}
+                Phone Case
+              </h1>
+              <p className="mt-8 max-w-prose text-balance text-center text-lg md:text-wrap lg:pr-10 lg:text-left">
+                Capture your favorite memories with your own,{" "}
+                <span className="font-semibold">one-of-one</span> phone case.
+                CaseCorner allows you to protect your memories, not just your
+                phone case.
+              </p>
+              <ul className="mt-8 flex flex-col items-center space-y-2 text-left font-medium sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    High-quality, durable material
+                  </li>
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />5 year
+                    print guarantee
+                  </li>
+                  <li className="flex items-center gap-1.5 text-left">
+                    <Check className="h-5 w-5 shrink-0 text-green-600" />
+                    Modern iphone models supported
+                  </li>
+                </div>
+              </ul>
+
+              <div className="mt-12 flex flex-col items-center gap-5 sm:flex-row sm:items-start">
+                <div className="flex -space-x-4">
+                  <img
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-1.png"
+                    alt="user image"
+                  />
+                  <img
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-2.png"
+                    alt="user image"
+                  />
+                  <img
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-3.png"
+                    alt="user image"
+                  />
+                  <img
+                    className="inline-block size-10 rounded-full ring-2 ring-slate-100"
+                    src="/users/user-4.jpg"
+                    alt="user image"
+                  />
+                </div>
+
+                <div className="flex flex-col items-center justify-between sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="size-4 fill-green-600 text-green-400" />
+                    <Star className="size-4 fill-green-600 text-green-400" />
+                    <Star className="size-4 fill-green-600 text-green-400" />
+                    <Star className="size-4 fill-green-600 text-green-400" />
+                    <Star className="size-4 fill-green-600 text-green-400" />
+                  </div>
+                  <p>
+                    <span className="font-semibold">1.250</span> happy customers
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full mt-32 flex h-fit w-full justify-center px-8 sm:px-16 md:px-0 lg:col-span-1 lg:mx-0 lg:mt-20">
+            <div className="mx-w-xl relative">
+              <img
+                src="/your-image.png"
+                className="absolute -top-20 left-56 hidden w-40 select-none sm:block lg:hidden lg:w-52 xl:block"
+                alt="user image"
+              />
+              <img
+                src="/line.png"
+                alt="line"
+                className="absolute -bottom-6 -left-6 w-full select-none"
+              />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg" />
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
+      <section>
+        <MaxWidthWrapper className="py-24">
+          <div className="mb-12 px-6 lg:px-8">
+            <div className="mx-auto max-w-2xl sm:text-center">
+              <h2 className="order-1 mt-2 text-balance text-center text-5xl font-bold !leading-tight tracking-tight text-gray-900 md:text-6xl">
+                Upload your photo and get{" "}
+                <span className="relative bg-green-600 px-2 text-white">
+                  your own case
+                </span>{" "}
+                now
+              </h2>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-6xl px-6 lg:px-8">
+            <div className="relative flex grid-cols-2 flex-col items-center gap-40 md:grid">
+              <img
+                src="/arrow.png"
+                className="absolute left-1/2 top-[25rem] z-10 -translate-x-1/2 -translate-y-1/2 rotate-90 md:top-1/2 md:rotate-0"
+                alt="img"
+              />
+
+              <div className="relative h-80 w-full max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 md:h-full md:justify-self-end lg:rounded-2xl">
+                <img
+                  src="/horse.jpg"
+                  className="h-full w-full rounded-md bg-white object-cover shadow-2xl ring-1 ring-gray-900/10"
+                />
+              </div>
+
+              <Phone className="w-60" imgSrc="/horse_phone.jpg" />
+            </div>
+          </div>
+
+          <ul className="mx-auto mt-12 w-fit max-w-prose space-y-2 sm:text-lg">
+            <li className="w-fit">
+              <Check className="mr-1.5 inline h-5 w-5 text-green-600" />
+              High-quality silicone material
+            </li>
+            <li className="w-fit">
+              <Check className="mr-1.5 inline h-5 w-5 text-green-600" />
+              Scratch- and fingerprint resistant coating
+            </li>
+            <li className="w-fit">
+              <Check className="mr-1.5 inline h-5 w-5 text-green-600" />
+              Wireless charging compatible
+            </li>
+            <li className="w-fit">
+              <Check className="mr-1.5 inline h-5 w-5 text-green-600" />5 year
+              print warranty
+            </li>
+
+            <div className="flex justify-center">
+              <Link
+                className={buttonVariants({
+                  size: "lg",
+                  className: "mx-auto mt-8",
+                })}
+                href="/configure/upload"
+              >
+                Create your case now
+                <ArrowRight className="ml-1.5 size-4" />
+              </Link>
+            </div>
+          </ul>
+        </MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
