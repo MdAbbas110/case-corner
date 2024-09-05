65aee93f2d4d50d1f69f6787525cf19d9605d165
nikunj <nikunjtiwari7@gmail.com>
2024-09-05 22:03:18 +0530

    created product detils page stored in design folder

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatPrice = (price: number) => {
+ const formatter = new Intl.NumberFormat("e-us",{
+style : 'currency',
+currency: 'USD'
+ })
+ return formatter.format(price)
+}
